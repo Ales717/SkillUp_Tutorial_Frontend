672a68a9548d3d92f81b3d7833c3a80ad56e633e
Ales717 <prosenjak.ales77@gmail.com>
2024-04-02 12:12:53 +0200

    Pages and Routes

---
+++ src/pages/Login.tsx
@@ -0,0 +1,8 @@
+import Layout from 'components/ui/Layout'
+import { FC } from 'react'
+
+const Login: FC = () => {
+  return <Layout>Login</Layout>
+}
+
+export default Login
---
+++ src/pages/Register.tsx
@@ -0,0 +1,9 @@
+import DashboardLayout from 'components/ui/DashboardLayout'
+import Layout from 'components/ui/Layout'
+import { FC } from 'react'
+
+const Register: FC = () => {
+  return <Layout>Register</Layout>
+}
+
+export default Register
