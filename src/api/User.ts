5974d21a43a4cffd3629853f1c9244cdef3ca081
Ales717 <prosenjak.ales77@gmail.com>
2024-04-04 10:13:22 +0200

    Users

--- src/api/User.ts
+++ src/api/User.ts
@@ -1,14 +1,21 @@
 import { apiRoutes } from 'constants/apiConstants'
-import { apiRequest } from './Api'
+import {
+  CreateUserFields,
+  UpdateUserFields,
+} from 'hooks/react-hook-form/useCreateUpdateUser'
 import { LoginUserFields } from 'hooks/react-hook-form/useLogin'
-import { UserType } from 'models/auth'
 import { RegisterUserFields } from 'hooks/react-hook-form/useRegister'
+import { UserType } from 'models/auth'
+import { apiRequest } from './Api'
 
 export const fetchUser = async () =>
   apiRequest<undefined, UserType>('get', apiRoutes.FETCH_USER)
 
-export const signout = async () =>
-  apiRequest<undefined, void>('post', apiRoutes.SIGNOUT)
+export const fetchUsers = async (pageNumber: number) =>
+  apiRequest<number, UserType[]>(
+    'get',
+    `${apiRoutes.FETCH_USERS}?page=${pageNumber}`,
+  )
 
 export const login = async (data: LoginUserFields) =>
   apiRequest<LoginUserFields, UserType>('post', apiRoutes.LOGIN, data)
@@ -16,9 +23,25 @@ export const login = async (data: LoginUserFields) =>
 export const register = async (data: RegisterUserFields) =>
   apiRequest<RegisterUserFields, void>('post', apiRoutes.SIGNUP, data)
 
+export const signout = async () =>
+  apiRequest<undefined, void>('post', apiRoutes.SIGNOUT)
+
 export const uploadAvatar = async (formData: FormData, id: string) =>
   apiRequest<FormData, void>(
     'post',
     `${apiRoutes.UPLOAD_AVATAR_IMAGE}/${id}`,
     formData,
   )
+
+export const createUser = async (data: CreateUserFields) =>
+  apiRequest<CreateUserFields, void>('post', apiRoutes.USERS_PREFIX, data)
+
+export const updateUser = async (data: UpdateUserFields, id: string) =>
+  apiRequest<UpdateUserFields, void>(
+    'patch',
+    `${apiRoutes.USERS_PREFIX}/${id}`,
+    data,
+  )
+
+export const deleteUser = async (id: string) =>
+  apiRequest<string, UserType>('delete', `${apiRoutes.USERS_PREFIX}/${id}`)
